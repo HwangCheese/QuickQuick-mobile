115504f95f9a4555e66e78b3eb3389a3d59c4580
Ahrin Jun <wjsdkfls03@naver.com>
2024-07-30 15:28:21 +0900

    Update login_screen.dart
    화면 터치로 키보드 숨기기

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -64,101 +64,106 @@ class _LogInState extends State<LogIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Padding(padding: EdgeInsets.only(top: 50)),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 50.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Sign In',
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(padding: EdgeInsets.only(top: 50)),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 35.0),
-              Form(
-                child: Container(
-                  padding: EdgeInsets.all(40.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextField(
-                        controller: controller,
-                        autofocus: true,
-                        decoration: InputDecoration(
-                          hintText: '전화번호',
-                          fillColor: Color(0xFFE2F1FF),
-                          filled: true,
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(
-                              horizontal: 24.0, vertical: 12.0),
-                          prefixIcon: Padding(
-                            padding: const EdgeInsets.only(left: 5.0),
-                            child: Icon(Icons.local_post_office_outlined,
-                                size: 24.0, color: Colors.grey),
-                          ),
-                        ),
-                        keyboardType: TextInputType.phone,
-                      ),
-                      SizedBox(height: 20.0),
-                      TextField(
-                        controller: controller2,
-                        decoration: InputDecoration(
-                          hintText: '비밀번호(4자리)',
-                          fillColor: Color(0xFFE2F1FF),
-                          filled: true,
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(
-                              horizontal: 24.0, vertical: 12.0),
-                          prefixIcon: Padding(
-                            padding: const EdgeInsets.only(left: 5.0),
-                            child: Icon(Icons.lock_outline,
-                                size: 24.0, color: Colors.grey),
-                          ),
-                        ),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      SizedBox(height: 40.0),
-                      Center(
-                        child: SizedBox(
-                          width: 180,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              loginUser(controller.text, controller2.text);
-                            },
-                            child: Text(
-                              '로그인',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                color: Colors.white,
-                              ),
+                const SizedBox(height: 35.0),
+                Form(
+                  child: Container(
+                    padding: EdgeInsets.all(40.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextField(
+                          controller: controller,
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            hintText: '전화번호',
+                            fillColor: Color(0xFFE2F1FF),
+                            filled: true,
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 24.0, vertical: 12.0),
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.only(left: 5.0),
+                              child: Icon(Icons.local_post_office_outlined,
+                                  size: 24.0, color: Colors.grey),
                             ),
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Color(0xFFCDE9FF),
-                                minimumSize: Size(double.infinity, 50),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                )),
+                          ),
+                          keyboardType: TextInputType.phone,
+                        ),
+                        SizedBox(height: 20.0),
+                        TextField(
+                          controller: controller2,
+                          decoration: InputDecoration(
+                            hintText: '비밀번호(4자리)',
+                            fillColor: Color(0xFFE2F1FF),
+                            filled: true,
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 24.0, vertical: 12.0),
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.only(left: 5.0),
+                              child: Icon(Icons.lock_outline,
+                                  size: 24.0, color: Colors.grey),
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 40.0),
+                        Center(
+                          child: SizedBox(
+                            width: 180,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                loginUser(controller.text, controller2.text);
+                              },
+                              child: Text(
+                                '로그인',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color(0xFFCDE9FF),
+                                  minimumSize: Size(double.infinity, 50),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  )),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
