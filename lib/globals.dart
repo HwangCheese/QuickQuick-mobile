311587be45a1431558b222ad562e4657c78be59d
Ahrin Jun <wjsdkfls03@naver.com>
2024-08-20 18:31:50 +0900

    Update globals.dart
    사용자 이름 추가

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,6 @@
 // 전역 변수들 저장하는 파일입니다.
 
 String USER_ID = "";
-const String SERVER_IP = 'http://192.168.75.31:3000';
+String USER_NAME = "";
+const String SERVER_IP = 'http://223.194.129.146:3000';
+const String apiKey = '';
