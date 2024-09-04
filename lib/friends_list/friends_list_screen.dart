3192617ca63828ce8212eb6fa9fa9444e128c979
Ahrin Jun <wjsdkfls03@naver.com>
2024-09-04 19:20:21 +0900

    Update friends_list_screen.dart
    친구 목록 검색 후 화면 터치 시 키보드 내려감

--- lib/friends_list/friends_list_screen.dart
+++ lib/friends_list/friends_list_screen.dart
@@ -264,74 +264,80 @@ class _FriendsListScreenState extends State<FriendsListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('친구 목록'),
-      ),
-      body: RefreshIndicator(
-        onRefresh: _refreshFriends,
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 60.0), // 검색창을 아래로 내리기 위한 여백
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 16.0), // 검색창과 리스트 사이의 간격
-                child: TextField(
-                  controller: _searchController,
-                  decoration: InputDecoration(
-                    hintText: '이름 검색',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(
-                        color: Color(0xFFE2F1FF), // 기본 상태 테두리 색상
-                        width: 3.0, // 테두리 굵기
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus(); // 키보드 내리기
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('친구 목록'),
+        ),
+        body: RefreshIndicator(
+          onRefresh: _refreshFriends,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 60.0), // 검색창을 아래로 내리기 위한 여백
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 16.0), // 검색창과 리스트 사이의 간격
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: '이름 검색',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Color(0xFFE2F1FF), // 기본 상태 테두리 색상
+                          width: 3.0, // 테두리 굵기
+                        ),
                       ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(
-                        color: Color(0xFFE2F1FF), // 기본 상태 테두리 색상
-                        width: 3.0, // 테두리 굵기
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Color(0xFFE2F1FF), // 기본 상태 테두리 색상
+                          width: 3.0, // 테두리 굵기
+                        ),
                       ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(
-                        color: Color(0xFFE2F1FF), // 포커스 상태 테두리 색상
-                        width: 3.0, // 테두리 굵기
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Color(0xFFE2F1FF), // 포커스 상태 테두리 색상
+                          width: 3.0, // 테두리 굵기
+                        ),
                       ),
+                      suffixIcon:
+                          Icon(Icons.search, color: Colors.grey), // 검색 아이콘
                     ),
-                    suffixIcon:
-                        Icon(Icons.search, color: Colors.grey), // 검색 아이콘
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: filteredFriends.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(filteredFriends[index]['user_name']!),
-                    onLongPress: () {
-                      _showActionDialog(filteredFriends[index]['user_name']!);
-                    },
-                  );
-                },
+              Expanded(
+                child: ListView.builder(
+                  itemCount: filteredFriends.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(filteredFriends[index]['user_name']!),
+                      onLongPress: () {
+                        _showActionDialog(filteredFriends[index]['user_name']!);
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddFriendDialog();
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Color(0xFFCDE9FF), // 버튼 배경 색상
-        shape: CircleBorder(), // 버튼 모양을 원으로 설정
-        elevation: 2.0, // 그림자 효과
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _showAddFriendDialog();
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Color(0xFFCDE9FF), // 버튼 배경 색상
+          shape: CircleBorder(), // 버튼 모양을 원으로 설정
+          elevation: 2.0, // 그림자 효과
+        ),
       ),
     );
   }
