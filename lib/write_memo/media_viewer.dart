16750c47404c3383f82281ef3754684622679b1b
Ahrin Jun <wjsdkfls03@naver.com>
2024-09-09 22:11:25 +0900

    Update media_viewer.dart
    - 비디오 뷰어 수정

--- lib/write_memo/media_viewer.dart
+++ lib/write_memo/media_viewer.dart
@@ -1,11 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'write_memo_screen.dart';
 import 'package:video_player/video_player.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'dart:io';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
 
 class MediaViewer extends StatefulWidget {
   final String filePath;
@@ -24,14 +20,11 @@ class _MediaViewerState extends State<MediaViewer> {
   bool _isAudio = false;
   bool _isVideo = false;
   bool _isImage = false;
-  bool _isPdf = false;
   Duration _duration = Duration.zero;
   Duration _position = Duration.zero;
   int _totalPages = 0;
   int _currentPage = 0;
   bool _isReady = false;
-  PDFViewController? _pdfViewController;
-  String _summary = '';
 
   @override
   void initState() {
@@ -44,7 +37,6 @@ class _MediaViewerState extends State<MediaViewer> {
     _isImage = widget.filePath.endsWith('.png') ||
         widget.filePath.endsWith('.jpg') ||
         widget.filePath.endsWith('.jpeg');
-    _isPdf = widget.filePath.endsWith('.pdf');
 
     if (_isAudio) {
       _initializeAudioPlayer();
@@ -55,7 +47,6 @@ class _MediaViewerState extends State<MediaViewer> {
 
   void _initializeAudioPlayer() {
     _audioPlayer = AudioPlayer();
-
     _audioPlayer.setSource(DeviceFileSource(widget.filePath)).then((_) {
       _audioPlayer.getDuration().then((duration) {
         setState(() {
@@ -132,13 +123,6 @@ class _MediaViewerState extends State<MediaViewer> {
     });
   }
 
-  String _formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return '$minutes:$seconds';
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -158,9 +142,7 @@ class _MediaViewerState extends State<MediaViewer> {
                           ? _buildVideoPlayer()
                           : _isImage
                               ? _buildImageView()
-                              : _isPdf
-                                  ? _buildPdfViewer()
-                                  : Container(),
+                              : Container(),
                 ),
               ),
             ),
@@ -168,33 +150,20 @@ class _MediaViewerState extends State<MediaViewer> {
               bottom: 30,
               left: 0,
               right: 0,
-              child: Column(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (_isAudio) ...[
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pop(context, 'transcribe');
-                          },
-                          child: Text('텍스트 변환'),
-                        ),
-                        SizedBox(width: 10),
-                      ],
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                          widget.onDelete();
-                        },
-                        child: Text('삭제'),
-                      ),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                        onPressed: () => Navigator.of(context).pop(),
-                        child: Text('닫기'),
-                      ),
-                    ],
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      widget.onDelete();
+                    },
+                    child: Text('삭제'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text('닫기'),
                   ),
                 ],
               ),
@@ -229,7 +198,7 @@ class _MediaViewerState extends State<MediaViewer> {
           inactiveColor: Colors.grey,
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -249,99 +218,86 @@ class _MediaViewerState extends State<MediaViewer> {
   }
 
   Widget _buildVideoPlayer() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
       children: [
-        Expanded(
+        Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.6,
           child: AspectRatio(
             aspectRatio: _videoController.value.aspectRatio,
             child: VideoPlayer(_videoController),
           ),
         ),
-        Slider(
-          value: _position.inSeconds.toDouble(),
-          max: _duration.inSeconds.toDouble(),
-          onChanged: (value) async {
-            final position = Duration(seconds: value.toInt());
-            await _videoController.seekTo(position);
-            if (!_isPlaying) {
-              _videoController.play();
-            }
-          },
-          activeColor: Colors.white,
-          inactiveColor: Colors.grey,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                _formatDuration(_position),
-                style: TextStyle(color: Colors.white),
+        Positioned(
+          top: 0,
+          bottom: -300,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: IconButton(
+              icon: Icon(
+                _isPlaying ? Icons.pause : Icons.play_arrow,
+                color: Colors.white,
+                size: 64,
               ),
-              Text(
-                _formatDuration(_duration),
-                style: TextStyle(color: Colors.white),
+              onPressed: _toggleVideoPlayback,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 20,
+          left: MediaQuery.of(context).size.width * 0.05, // 슬라이더의 위치 조정
+          right: MediaQuery.of(context).size.width * 0.05,
+          child: Column(
+            children: [
+              Slider(
+                value: _position.inSeconds.toDouble(),
+                max: _duration.inSeconds.toDouble(),
+                onChanged: (value) async {
+                  final position = Duration(seconds: value.toInt());
+                  await _videoController.seekTo(position);
+                  if (!_isPlaying) {
+                    _videoController.play();
+                  }
+                },
+                activeColor: Colors.white,
+                inactiveColor: Colors.grey,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      _formatDuration(_position),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      _formatDuration(_duration),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
-        IconButton(
-          icon: Icon(
-            _isPlaying ? Icons.pause : Icons.play_arrow,
-            color: Colors.white,
-            size: 64,
-          ),
-          onPressed: _toggleVideoPlayback,
-        ),
       ],
     );
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return [if (duration.inHours > 0) hours, minutes, seconds].join(':');
   }
 
   Widget _buildImageView() {
     return Image.file(
       File(widget.filePath),
       fit: BoxFit.contain,
-    );
-  }
-
-  Widget _buildPdfViewer() {
-    return PDFView(
-      filePath: widget.filePath,
-      enableSwipe: true,
-      swipeHorizontal: false, // 위아래로 스크롤하게 설정
-      autoSpacing: true,
-      pageFling: true, // 자연스러운 스크롤을 위해 설정
-      fitEachPage: false, // 페이지가 전체 화면에 맞도록 조정
-      fitPolicy: FitPolicy.BOTH, // 페이지를 가로와 세로 모두 맞춤
-      onRender: (_pages) {
-        setState(() {
-          _totalPages = _pages ?? 0;
-          _isReady = true;
-        });
-      },
-      onViewCreated: (PDFViewController pdfViewController) {
-        _pdfViewController = pdfViewController;
-        _pdfViewController!.setPage(0); // 첫 번째 페이지로 설정
-      },
-      onPageChanged: (int? page, int? total) {
-        setState(() {
-          _currentPage = page ?? 0;
-        });
-      },
-      onError: (error) {
-        print("Error loading PDF: $error");
-        setState(() {
-          _isReady = false;
-        });
-      },
-      onPageError: (page, error) {
-        print("Page error on page $page: $error");
-        setState(() {
-          _isReady = false;
-        });
-      },
     );
   }
 }
