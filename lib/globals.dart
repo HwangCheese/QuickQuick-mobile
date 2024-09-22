a72533d1d7581ea84d1a86140cf9f9f8234848b6
Ahrin Jun <wjsdkfls03@naver.com>
2024-09-22 15:08:40 +0900

    Delete lib/globals.dart

--- lib/globals.dart
+++
@@ -1,7 +0,0 @@
-// 전역 변수들 저장하는 파일입니다.
-
-String USER_ID = "";
-String USER_NAME = "";
-const String SERVER_IP = 'http://223.194.129.146:3000';
-const String apiKey = '';
-const String openapiKey = '';
