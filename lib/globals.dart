3a1b101c960d4e0f2f91f9f2ff0f6d4dec9a724d
Ahrin Jun <wjsdkfls03@naver.com>
2024-09-22 15:09:42 +0900

    Create globals.dart

---
+++ lib/globals.dart
@@ -0,0 +1,7 @@
+// 전역 변수들 저장하는 파일입니다.
+
+String USER_ID = "";
+String USER_NAME = "";
+const String SERVER_IP = 'http://223.194.129.146:3000';
+const String apiKey = '';
+const String openapiKey = '';
